642df1f75b23879e7f3362906dc1f6975419f882
chukwumaonyia <127945859+chukwumaonyia@users.noreply.github.com>
2023-05-09 12:36:36 +0100

    Update HOME.jsx
    last commit

--- src/componet/HOME.jsx
+++ src/componet/HOME.jsx
@@ -11,7 +11,7 @@ const HOME = () => {
     <p className='text-pink-600'>Hi, my name is</p>
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Onyia Kelly </h1>
     <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer</h2>
-    <p className='text-[#8892b0] py-4 max-w-[700px]'> i'm a Front End Developer specializing in building (and occasionally desiging)
+    <p className='text-[#8892b0] py-4 max-w-[700px]'> I'm a Front End Developer specializing in building (and occasionally desiging)
         exceptional digital experinces. Currently ,i am fouced on buliding responsive web applications.</p>
 <div>
     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'> view work 
