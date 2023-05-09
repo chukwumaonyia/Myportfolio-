15585a1b34a6a9e7703fd580fd305328ca144e87
chukwumaonyia <127945859+chukwumaonyia@users.noreply.github.com>
2023-05-09 09:48:17 +0100

    RUN BUILD

--- src/componet/contact.jsx
+++ src/componet/contact.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-   <form method='POST' action="https://getform.io/f/e68eb525-894d-45c9-ba32-964958808e07" className='flex flex-col max-w-[600px] w-full'>
-    <div className='pb-8'>
-        <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>contact</p>
-        <p className='text-gray-300'>//submit the form below or shoot me an email.@myGmail.com </p>
-    </div>
-    <input className=' bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-    <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='Email' />
-    <textarea className=' bg-[#ccd6f6] p-2' name='massage' rows="10" placeholder='massage' ></textarea>
-    <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'> lets collaborate</button>
-   </form>
+      <form method='POST' action="https://getform.io/f/e68eb525-894d-45c9-ba32-964958808e07" className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>contact</p>
+          <p className='text-gray-300'>//submit the form below or shoot me an email.@myGmail.com </p>
+        </div>
+        <input className=' bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='Email' />
+        <textarea className=' bg-[#ccd6f6] p-2' name='massage' rows="10" placeholder='massage' ></textarea>
+        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'> lets collaborate</button>
+      </form>
 
     </div>
   )
