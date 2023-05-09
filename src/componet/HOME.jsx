9c83b8f2db982336e8b0fc97ae2b8b5d120e3d3f
chukwumaonyia <127945859+chukwumaonyia@users.noreply.github.com>
2023-05-09 12:38:50 +0100

    last  commit on I'm

--- src/componet/HOME.jsx
+++ src/componet/HOME.jsx
@@ -12,7 +12,7 @@ const HOME = () => {
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Onyia Kelly </h1>
     <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer</h2>
     <p className='text-[#8892b0] py-4 max-w-[700px]'> I'm a Front End Developer specializing in building (and occasionally desiging)
-        exceptional digital experinces. Currently ,i am fouced on buliding responsive web applications.</p>
+        exceptional digital experinces. Currently ,I am fouced on buliding responsive web applications.</p>
 <div>
     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'> view work 
     <span className='group-hover:rotate-90 duration-300'>
