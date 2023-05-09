a303bb94d7191f1a960e9598c05b11f165aa72bd
chukwumaonyia <127945859+chukwumaonyia@users.noreply.github.com>
2023-05-09 12:29:35 +0100

    Update HOME.jsx

--- src/componet/HOME.jsx
+++ src/componet/HOME.jsx
@@ -10,7 +10,7 @@ const HOME = () => {
 <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
     <p className='text-pink-600'>Hi, my name is</p>
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Onyia Kelly </h1>
-    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>i'm a Front End Developer</h2>
+    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer</h2>
     <p className='text-[#8892b0] py-4 max-w-[700px]'> i'm a Front End Developer specializing in building (and occasionally desiging)
         exceptional digital experinces. Currently ,i am fouced on buliding responsive web applications.</p>
 <div>
